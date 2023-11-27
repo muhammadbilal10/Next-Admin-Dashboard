f10cfd9269e6df969d7da4db5869926ac9f7e1a5
muhammadbilal10 <muhdbilal81@gmail.com>
2023-11-28 01:39:16 +0500

    feat: Add Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import SidebarNavigation from "@/components/SidebarNavigation";
+import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -19,24 +20,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="grid grid-cols-12">
-            {/* <Header /> */}
-            <div className="max-sm:hidden sm:col-span-2">
-              <SidebarNavigation />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="grid grid-cols-12">
+              {/* <Header /> */}
+              <div className="max-sm:hidden sm:col-span-2">
+                <SidebarNavigation />
+              </div>
+              <div className="sm:col-span-10 col-span-12">{children}</div>
+              <Toaster />
             </div>
-            <div className="sm:col-span-10 col-span-12">{children}</div>
-            <Toaster />
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/SidebarNavigation.tsx
+++ components/SidebarNavigation.tsx
@@ -2,15 +2,28 @@ import Image from "next/image";
 import cable from "@/public/cable.svg";
 import React from "react";
 import Link from "next/link";
-import { Home } from "lucide-react";
+import { Home, User } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
 
 const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "Users", href: "/user", current: false },
-  { name: "Resources", href: "#", current: false },
-  { name: "Company Directory", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Settings", href: "#", current: false },
+  { name: "Home", href: "/", current: true, icon: <Home />, child: [] },
+  {
+    name: "Users",
+    href: "/user",
+    current: false,
+    icon: <User />,
+    child: ["All User", "Add Fee"],
+  },
+  // { name: "Resources", href: "#", current: false, icon: Home },
+  // { name: "Company Directory", href: "#", current: false, icon: Home },
+  // { name: "Calendar", href: "#", current: false, icon: Home },
+  // { name: "Settings", href: "#", current: false, icon: Home },
 ];
 
 const SidebarNavigation = () => {
@@ -19,19 +32,44 @@ const SidebarNavigation = () => {
       <div>
         <Image src={cable} className="" alt="Logo" width={50} height={42} />
       </div>
+
       <nav>
         <ul className="">
-          {navigation.map((item, index) => (
+          <Accordion type="single" collapsible className="w-full">
+            {navigation.map((item, index) => (
+              <AccordionItem key={index} value={item.name}>
+                <AccordionTrigger className="flex p-2 my-2 text-gray-600 gap-x-3 rounded-md focus:bg-gray-50 focus:text-black font-semibold hover:no-underline">
+                  <span className="">{item.icon}</span>
+                  <span className="">{item.name}</span>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <ul className="flex flex-col items-center gap-y-2">
+                    {item.child.map((child, index) => (
+                      <li key={index} className="">
+                        <Link
+                          href={item.href}
+                          className="p-2 my-2 text-gray-600 gap-x-3 rounded-md focus:bg-gray-50 focus:text-black"
+                        >
+                          <span className="">{child}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+          {/* {navigation.map((item, index) => (
             <li key={index} className="">
               <Link
                 href={item.href}
                 className="flex p-2 my-2 text-gray-600 gap-x-3 rounded-md focus:bg-gray-50 focus:text-black font-semibold"
               >
-                <Home />
+                {item.icon}
                 <span className="">{item.name}</span>
               </Link>
             </li>
-          ))}
+          ))} */}
         </ul>
       </nav>
     </div>
